f217200f4e0abdd5af03eab95b10f82c7ce9ab3c
kainy01 <kainykawai@gmail.com>
2023-04-07 22:53:59 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(gifts, name) {
+    let arr=[]
+    for (let i = 0; i < gifts.length; i++) {
+      arr.push(`Thank you, ${gifts[i]}, for the wonderful ${name} gift!`);
+    }
+    return arr
+  }
+
+
+  function countDown(countup) {
+    while (countup !==-1) {
+      console.log(countup--);
+    }
+  }
